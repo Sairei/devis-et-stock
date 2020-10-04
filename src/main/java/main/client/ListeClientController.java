5f63c0e4c1b3236403bb8f86f5df45ea389924b9
Sairei <christophercauet@hotmail.fr>
2020-10-04 17:29:29 +0200

    mis à jour liste devis / client

--- src/main/java/main/client/ListeClientController.java
+++ src/main/java/main/client/ListeClientController.java
@@ -1,9 +1,86 @@
 package main.client;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import main.base.SQLDelete;
+import main.base.SQLSelect;
+import main.common.Constantes;
 
 public class ListeClientController {
 	@FXML
+	private ListView<ClientDAO> listeClient;
+	@FXML
 	private Label nbClientTotal;
+	@FXML
+	private Button supprimerClient;
+	@FXML
+	private Button modifierClient;
+	
+	@FXML
+	public void initialize() {		
+		supprimerClient.disableProperty().bind(Bindings.isEmpty(listeClient.getSelectionModel().getSelectedItems()));
+		modifierClient.disableProperty().bind(Bindings.isEmpty(listeClient.getSelectionModel().getSelectedItems()));
+		
+		updateListe();
+	}
+	
+	@FXML
+	public void addClient() {
+		try {
+			Stage stage = new Stage();
+		    Parent root = FXMLLoader.load(getClass().getResource("/client/nouveauClient.fxml"));
+		    stage.setScene(new Scene(root));
+		    stage.setTitle("Enregistrement d'un nouveau client");
+		    stage.initModality(Modality.APPLICATION_MODAL);
+		    stage.showAndWait();
+		    
+		    updateListe();
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void delClient() {
+		ClientDAO client = listeClient.getSelectionModel().getSelectedItem();
+		
+		SQLDelete.deleteClientById(client.getId());
+
+		updateListe();
+	}
+	
+	private void updateListe() {
+		listeClient.getItems().removeAll(listeClient.getItems());
+		
+		ResultSet allClient = SQLSelect.selectAllClient();
+		try {
+			if(null == allClient || !allClient.next()) {
+				listeClient.setPlaceholder(new Label(Constantes.aucun_client));
+			} else {
+				do {
+					ClientDAO client = new ClientDAO(allClient.getLong(Constantes.colClientId), allClient.getString(Constantes.colClientGenre), 
+							allClient.getString(Constantes.colClientNom), allClient.getString(Constantes.colClientPrenom), 
+							allClient.getString(Constantes.colClientAdresse), allClient.getString(Constantes.colClientMail), 
+							allClient.getString(Constantes.colClientTelephone), allClient.getLong(Constantes.colClientIdMensuration));
+					listeClient.getItems().add(client);
+				} while(allClient.next());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/main/devis/ListeDevisController.java
+++ src/main/java/main/devis/ListeDevisController.java
@@ -1,9 +1,74 @@
 package main.devis;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import main.base.SQLSelect;
+import main.common.Constantes;
 
 public class ListeDevisController {
 	@FXML
+	private ListView<DevisDAO> listeDevis;
+	@FXML
 	private Label nbDevisTotal;
+	@FXML
+	private Button supprimerDevis;
+	@FXML
+	private Button modifierDevis;
+	
+	@FXML
+	public void initialize() {
+		supprimerDevis.disableProperty().bind(Bindings.isEmpty(listeDevis.getSelectionModel().getSelectedItems()));
+		modifierDevis.disableProperty().bind(Bindings.isEmpty(listeDevis.getSelectionModel().getSelectedItems()));
+		
+		updateListe();
+	}
+	
+	@FXML
+	public void addDevis() {
+		try {
+			Stage stage = new Stage();
+		    Parent root = FXMLLoader.load(getClass().getResource("/devis/nouveauDevis.fxml"));
+		    stage.setScene(new Scene(root));
+		    stage.setTitle("Création d'un nouveau devis");
+		    stage.initModality(Modality.APPLICATION_MODAL);
+		    stage.showAndWait();
+		    
+		    updateListe();
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void updateListe() {
+		listeDevis.getItems().removeAll(listeDevis.getItems());
+		
+		ResultSet allDevis = SQLSelect.selectAllDevis();
+		try {
+			if(null == allDevis || !allDevis.next()) {
+				listeDevis.setPlaceholder(new Label(Constantes.aucun_devis));
+			} else {
+				do {
+					DevisDAO devis = new DevisDAO(allDevis.getString(Constantes.colDevisDateCrea), allDevis.getString(Constantes.colDevisTitre), 
+							allDevis.getString(Constantes.colDevisDescription), allDevis.getLong(Constantes.colDevisIdClient));
+				    listeDevis.getItems().add(devis);
+				} while(allDevis.next());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
